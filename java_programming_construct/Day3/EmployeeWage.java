12cee02bd573a64df777080b5d0d0ae9832d9751
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-28 20:57:37 +0530

    UC1:Employee is Present or Absent

--- java_programming_construct/Day3/EmployeeWage.java
+++ java_programming_construct/Day3/EmployeeWage.java
@@ -2,6 +2,11 @@ package Day3;
 
 public class EmployeeWage {
     public static void main(String args[]){
-        System.out.println("Bharati Welcome to Employee Wage Computation");
+        final int IS_PRESENT=1;
+        int empCheck=(int)(Math.floor(Math.random()*10))%2;
+        if(empCheck==IS_PRESENT)
+            System.out.print("Employee is present");
+        else
+            System.out.print("Employee is not present");
     }
 }
