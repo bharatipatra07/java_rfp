beb74dd85793de787a8eadfba4851670369c7bda
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-28 20:36:15 +0530

    UC1:Welcome to Employee Wage Computation

---
+++ java_programming_construct/Day3/EmployeeWage.java
@@ -0,0 +1,12 @@
+package java_programming_construct.java_fundamental;
+
+import java.util.Scanner;
+
+public class EmployeeWage {
+    //EMPLOYEE WAGE Start
+        public static void main(String args[]){
+
+            System.out.print("Bharati Welcome to Employee Wage Computation ");
+        }
+    }
+
