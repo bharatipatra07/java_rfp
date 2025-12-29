21cac81b085db9c9e9954dbcb294ab8ec84ebe41
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 10:54:53 +0530

    UC2:Daily Employee Wage

--- java_programming_construct/Day3/EmployeeWage.java
+++ java_programming_construct/Day3/EmployeeWage.java
@@ -2,11 +2,13 @@ package Day3;
 
 public class EmployeeWage {
     public static void main(String args[]){
+        final int EMP_RATE_PER_HOUR=20;
         final int IS_PRESENT=1;
+        int empHrs=0,empWage=0;
         int empCheck=(int)(Math.floor(Math.random()*10))%2;
         if(empCheck==IS_PRESENT)
-            System.out.print("Employee is present");
-        else
-            System.out.print("Employee is not present");
+            empHrs=8;
+        empWage=empHrs*EMP_RATE_PER_HOUR;
+        System.out.println(empWage);
     }
 }
