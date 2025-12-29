231a16780bf6e5533ef15c22833be01d7d74e3a6
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 11:35:14 +0530

    UC1:Implement Snake and Ladder starting at position 0

---
+++ java_programming_construct/Day4/SnakeLadder.java
@@ -0,0 +1,11 @@
+package java_programming_construct.programming_construct;
+
+
+public class SnakeLadder{
+    //UC 1
+    static class UC1 {
+        public static void main(String[] args) {
+            int dice = (int)(Math.random() * 6) + 1;
+            System.out.println("Dice rolled: " + dice);
+        }
+    }
