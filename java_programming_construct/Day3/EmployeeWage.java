b5e3726fa3597212a45c720839687612a49863aa
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 11:04:06 +0530

    UC5:Calculate Wages for a Month

--- java_programming_construct/Day3/EmployeeWage.java
+++ java_programming_construct/Day3/EmployeeWage.java
@@ -1,20 +1,38 @@
 package Day3;
 
 public class EmployeeWage {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         final int IS_PART_TIME = 1;
         final int IS_FULL_TIME = 2;
         final int EMP_RATE_PER_HOUR = 20;
-        int empHrs=0;
-        int empWage;
-        int empCheck=(int)(Math.floor(Math.random()*10))%3;
-        switch(empCheck){
-            case IS_PART_TIME:empHrs=4;break;
-            case IS_FULL_TIME:empHrs=8;break;
-            default:empHrs=0;
-        }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
+        final int NUM_OF_WORKING_DAYS = 20;
+        int empHrs = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
+        for (int day=0;day< NUM_OF_WORKING_DAYS;day++) {
+            // FIXED random logic
+            int emp_check = (int) (Math.random() * 10) % 3;
 
-        System.out.println("Employee Wage: " + empWage);
+            switch (emp_check) {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+            }
+
+            totalEmpHrs += empHrs;
+
+            System.out.println("Day: " + totalWorkingDays + " Emp Hrs: " + empHrs
+            );
+        }
+
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
