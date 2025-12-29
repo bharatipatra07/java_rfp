864c7f4c19993836956fecc577af95ed12da0445
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 12:11:59 +0530

    UC6-UC7:Add dice count, positions per roll, and 2-player game with winner

--- java_programming_construct/Day4/SnakeLadder.java
+++ java_programming_construct/Day4/SnakeLadder.java
@@ -2,23 +2,36 @@ package java_programming_construct.programming_construct;
 
 
 public class SnakeLadder{
-    //UC 5
+    //UC6 and UC7
     public static void main(String[] args) {
-        int position = 0, diceCount = 0;
-        int WIN = 100;
+        int p1 = 0, p2 = 0, WIN = 100, diceCount = 0;
+        boolean p1Turn = true;
 
-        while(position < WIN) {
+        while(p1 < WIN && p2 < WIN) {
             int dice = (int)(Math.random() * 6) + 1;
+            int option = (int)(Math.random() * 3);
             diceCount++;
 
-            int option = (int)(Math.random() * 3);
-            switch(option) {
-                case 1: if(position + dice <= WIN) position += dice; break;
-                case 2: position -= dice; if(position < 0) position = 0; break;
+            if(p1Turn) {
+                switch(option) {
+                    case 1: if(p1 + dice <= WIN) p1 += dice; break;
+                    case 2: p1 -= dice; if(p1 < 0) p1 = 0; break;
+                }
+                System.out.println("Player 1 dice: " + dice + ", position: " + p1);
+                if(option != 1) p1Turn = false;
+            } else {
+                switch(option) {
+                    case 1: if(p2 + dice <= WIN) p2 += dice; break;
+                    case 2: p2 -= dice; if(p2 < 0) p2 = 0; break;
+                }
+                System.out.println("Player 2 dice: " + dice + ", position: " + p2);
+                if(option != 1) p1Turn = true;
             }
-
-            System.out.println("Dice: " + dice + ", Position: " + position);
         }
-        System.out.println("Player reached 100 Total dice rolls: " + diceCount);
+
+        if(p1 == WIN) System.out.println("Player 1 WON");
+        else System.out.println("Player 2 WON");
+
+        System.out.println("Total dice rolls: " + diceCount);
     }
 }
