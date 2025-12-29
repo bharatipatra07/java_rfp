b440c5e79b677310592c0b89dcd9b59a57cd8338
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 11:44:07 +0530

    UC2: Add dice roll (1–6)

--- java_programming_construct/Day4/SnakeLadder.java
+++ java_programming_construct/Day4/SnakeLadder.java
@@ -2,10 +2,9 @@ package java_programming_construct.programming_construct;
 
 
 public class SnakeLadder{
-    //UC 1
-    static class UC1 {
-        public static void main(String[] args) {
-            int dice = (int)(Math.random() * 6) + 1;
-            System.out.println("Dice rolled: " + dice);
-        }
+    //UC 2
+    public static void main(String[] args) {
+        int dice = (int)(Math.random() * 6) + 1;
+        System.out.println("Player rolled dice: " + dice);
+    }
     }
