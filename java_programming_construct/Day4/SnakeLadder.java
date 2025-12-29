c994c900b5b8aa6e02eaf76bbfe2f97c9c77dd58
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 12:01:55 +0530

    UC5:player reaches exact position 100

--- java_programming_construct/Day4/SnakeLadder.java
+++ java_programming_construct/Day4/SnakeLadder.java
@@ -2,27 +2,23 @@ package java_programming_construct.programming_construct;
 
 
 public class SnakeLadder{
-    //UC 4
+    //UC 5
     public static void main(String[] args) {
-        int position = 0;
+        int position = 0, diceCount = 0;
         int WIN = 100;
 
         while(position < WIN) {
             int dice = (int)(Math.random() * 6) + 1;
-            int option = (int)(Math.random() * 3);
+            diceCount++;
 
+            int option = (int)(Math.random() * 3);
             switch(option) {
-                case 0: break; // No Play
-                case 1: // Ladder
-                    if(position + dice <= WIN) position += dice;
-                    break;
-                case 2: // Snake
-                    position -= dice;
-                    if(position < 0) position = 0;
-                    break;
+                case 1: if(position + dice <= WIN) position += dice; break;
+                case 2: position -= dice; if(position < 0) position = 0; break;
             }
+
             System.out.println("Dice: " + dice + ", Position: " + position);
         }
-        System.out.println("Player reached 100! Won the game.");
+        System.out.println("Player reached 100 Total dice rolls: " + diceCount);
     }
 }
