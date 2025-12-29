0799d376c65e2c0f0c79e0ef5e29ad113e78813c
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 11:17:30 +0530

    UC1:Implement line length using (x1,y1) and (x2,y2)

--- java_programming_construct/Day3/LineComparison.java
+++ java_programming_construct/Day3/LineComparison.java
@@ -1,7 +1,18 @@
 package Day3;
 
 public class LineComparison {
-    public static void main(String args[]){
-        System.out.println("Welcome to Line Comparison Computation");
+    public static void main(String[] args) {
+        System.out.println("Enter Line points");
+        Scanner sc=new Scanner(System.in);
+        int x1=sc.nextInt();
+        int y1=sc.nextInt();
+        int x2=sc.nextInt();
+        int y2=sc.nextInt();
+        double length = Math.sqrt(
+                Math.pow(x2 - x1, 2) +
+                        Math.pow(y2 - y1, 2)
+        );
+
+        System.out.println("Length of the line: " + length);
     }
 }
