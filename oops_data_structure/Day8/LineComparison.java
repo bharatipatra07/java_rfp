b0a1a2e2d704e5297805654cce1c600a3c0b442a
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 16:28:36 +0530

    START:Line Comparison using OOPS

---
+++ oops_data_structure/Day8/LineComparison.java
@@ -0,0 +1,7 @@
+package oops_data_structure.Day8;
+
+public class LineComparison {
+    public static void main(String args[]){
+        System.out.println("Welcome to Line Comparison Computation Program using OOPS");
+    }
+}
