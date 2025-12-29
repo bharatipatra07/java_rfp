39019351b340ed53e1d7eb24a971542457a9e9ab
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 11:02:00 +0530

    UC4:Convert Using Switch Case

--- java_programming_construct/Day3/EmployeeWage.java
+++ java_programming_construct/Day3/EmployeeWage.java
@@ -8,15 +8,11 @@ public class EmployeeWage {
         int empHrs=0;
         int empWage;
         int empCheck=(int)(Math.floor(Math.random()*10))%3;
-
-        if (empCheck == IS_PART_TIME) {
-            empHrs = 4;
-        } else if (empCheck == IS_FULL_TIME) {
-            empHrs = 8;
-        } else {
-            empHrs = 0;
+        switch(empCheck){
+            case IS_PART_TIME:empHrs=4;break;
+            case IS_FULL_TIME:empHrs=8;break;
+            default:empHrs=0;
         }
-
         empWage = empHrs * EMP_RATE_PER_HOUR;
 
         System.out.println("Employee Wage: " + empWage);
