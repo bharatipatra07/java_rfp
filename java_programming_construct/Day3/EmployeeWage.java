4aed5ee165a341f2f22551610105db139e4dec74
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 10:58:53 +0530

    UC3:Add Part Time Employee Wage

--- java_programming_construct/Day3/EmployeeWage.java
+++ java_programming_construct/Day3/EmployeeWage.java
@@ -2,13 +2,23 @@ package Day3;
 
 public class EmployeeWage {
     public static void main(String args[]){
-        final int EMP_RATE_PER_HOUR=20;
-        final int IS_PRESENT=1;
-        int empHrs=0,empWage=0;
-        int empCheck=(int)(Math.floor(Math.random()*10))%2;
-        if(empCheck==IS_PRESENT)
-            empHrs=8;
-        empWage=empHrs*EMP_RATE_PER_HOUR;
-        System.out.println(empWage);
+        final int IS_PART_TIME = 1;
+        final int IS_FULL_TIME = 2;
+        final int EMP_RATE_PER_HOUR = 20;
+        int empHrs=0;
+        int empWage;
+        int empCheck=(int)(Math.floor(Math.random()*10))%3;
+
+        if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+        } else if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+        } else {
+            empHrs = 0;
+        }
+
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+
+        System.out.println("Employee Wage: " + empWage);
     }
 }
