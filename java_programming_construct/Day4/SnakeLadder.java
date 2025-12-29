44e72cc2c3f08c01414dc109bb156582e6057054
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-29 11:55:53 +0530

    UC3:Add option check: No Play, Ladder, Snake

--- java_programming_construct/Day4/SnakeLadder.java
+++ java_programming_construct/Day4/SnakeLadder.java
@@ -2,9 +2,25 @@ package java_programming_construct.programming_construct;
 
 
 public class SnakeLadder{
-    //UC 2
+    //UC 3
     public static void main(String[] args) {
+        int position = 0;
         int dice = (int)(Math.random() * 6) + 1;
-        System.out.println("Player rolled dice: " + dice);
+        int option = (int)(Math.random() * 3);
+
+        switch(option) {
+            case 0:
+                System.out.println("No Play. Position stays: " + position);
+                break;
+            case 1:
+                position += dice;
+                System.out.println("Ladder Moved forward by " + dice + ". New position: " + position);
+                break;
+            case 2:
+                position -= dice;
+                if(position < 0) position = 0;
+                System.out.println("Snake Moved backward by " + dice + ". New position: " + position);
+                break;
+        }
     }
-    }
+}
