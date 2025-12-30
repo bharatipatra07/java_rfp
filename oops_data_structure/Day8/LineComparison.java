d31d5b31e1fa478fc494206d493ac237d76ffffe
bharatipatra07 <bharati.patra@bridgelabz.com>
2025-12-30 16:34:27 +0530

    UC1:Implement line length calculation using OOP

--- oops_data_structure/Day8/LineComparison.java
+++ oops_data_structure/Day8/LineComparison.java
@@ -1,7 +1,48 @@
-package oops_data_structure.Day8;
+// Point.java
+class Point {
+    private double x, y;
 
-public class LineComparison {
-    public static void main(String args[]){
-        System.out.println("Welcome to Line Comparison Computation Program using OOPS");
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
     }
 }
+
+ class Line {
+    private Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public double length() {
+        double dx = end.getX() - start.getX();
+        double dy = end.getY() - start.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        return "Line[" + start + " to " + end + "] Length=" + length();
+    }
+}
+
+
+public class LineComparison {
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(4, 6);
+        Line line = new Line(p1, p2);
+        System.out.println(line);
+    }
+}
+
