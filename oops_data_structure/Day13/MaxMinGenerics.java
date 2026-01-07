136af9e31b45a222b83ad581034a005006a3396e
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-07 16:06:11 +0530

    UC1_TC1.1:Text Data Max Number at 1st position

--- oops_data_structure/Day13/MaxMinGenerics.java
+++ oops_data_structure/Day13/MaxMinGenerics.java
@@ -12,6 +12,7 @@ public class MaxMinGenerics{
     }
 
     public static void main(String[] args) {
-
+        // TC 1.1 – Max at 1st position
+        System.out.println(testMaximum(30, 20, 10));
     }
 }
