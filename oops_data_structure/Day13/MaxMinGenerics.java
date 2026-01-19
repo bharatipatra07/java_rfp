7ca2ecf3424a81cdddd6d3b99cae51004e7a2368
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-19 15:03:17 +0530

    UC4:max method to take more than 3 parameters

--- oops_data_structure/Day13/MaxMinGenerics.java
+++ oops_data_structure/Day13/MaxMinGenerics.java
@@ -1,45 +1,16 @@
-class MaxMinLogic {
 
-    public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
-        T max = a;
+import java.util.Arrays;
 
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-}
-public class MaxMinGenerics<T extends Comparable<T>> {
+public class MaxMinGenerics {
 
-    private T a;
-    private T b;
-    private T c;
-
-    public MaxMinGenerics(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public T testMaximum() {
-        return MaxMinLogic.testMaximum(a, b, c);
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
 
     public static void main(String[] args) {
 
-        MaxMinGenerics<Integer> intTest =
-                new MaxMinGenerics<>(10, 20, 30);
-        System.out.println(intTest.testMaximum());
-
-        MaxMinGenerics<Float> floatTest =
-                new MaxMinGenerics<>(3.5f, 2.5f, 1.5f);
-        System.out.println(floatTest.testMaximum());
-
-        MaxMinGenerics<String> stringTest =
-                new MaxMinGenerics<>("Apple", "Peach", "Banana");
-        System.out.println(stringTest.testMaximum());
+        System.out.println(testMaximum(10, 40, 20, 30));
+        System.out.println(testMaximum("Apple", "Banana", "Peach", "Orange"));
     }
 }
