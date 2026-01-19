27777908a490d8b8fd55c6bf180c4dc813cbf54a
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-19 14:59:12 +0530

    Refactor_2(Uc1-Uc3) using Generic class

--- oops_data_structure/Day13/MaxMinGenerics.java
+++ oops_data_structure/Day13/MaxMinGenerics.java
@@ -1,4 +1,4 @@
-public class MaxMinGenerics {
+class MaxMinLogic {
 
     public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
         T max = a;
@@ -11,10 +11,35 @@ public class MaxMinGenerics {
         }
         return max;
     }
+}
+public class MaxMinGenerics<T extends Comparable<T>> {
+
+    private T a;
+    private T b;
+    private T c;
+
+    public MaxMinGenerics(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T testMaximum() {
+        return MaxMinLogic.testMaximum(a, b, c);
+    }
 
     public static void main(String[] args) {
-        System.out.println(testMaximum(10, 20, 30));
-        System.out.println(testMaximum(3.5f, 2.5f, 1.5f));
-        System.out.println(testMaximum("Apple", "Peach", "Banana"));
+
+        MaxMinGenerics<Integer> intTest =
+                new MaxMinGenerics<>(10, 20, 30);
+        System.out.println(intTest.testMaximum());
+
+        MaxMinGenerics<Float> floatTest =
+                new MaxMinGenerics<>(3.5f, 2.5f, 1.5f);
+        System.out.println(floatTest.testMaximum());
+
+        MaxMinGenerics<String> stringTest =
+                new MaxMinGenerics<>("Apple", "Peach", "Banana");
+        System.out.println(stringTest.testMaximum());
     }
 }
