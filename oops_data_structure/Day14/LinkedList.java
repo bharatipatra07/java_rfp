e695db3801cca25423c862b45f54403e5d905022
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-19 15:12:50 +0530

    UC1:Simple LinkedList of Node 56,30 and 70

---
+++ oops_data_structure/Day14/LinkedList.java
@@ -0,0 +1,16 @@
+ class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class LinkedList{
+    public static void main(String args[]){
+        Node head=new Node(56);
+        Node Second=new Node(30);
+        Node Third=new Node(70);
+    }
+}
