022d08f5631c7d9d191392ac0bb5f1606349819c
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-19 15:07:10 +0530

    UC5: Extend generic max method to print maximum value to stdout and invoke via testMaximum

--- oops_data_structure/Day13/MaxMinGenerics.java
+++ oops_data_structure/Day13/MaxMinGenerics.java
@@ -1,16 +1,24 @@
-
-import java.util.Arrays;
-
 public class MaxMinGenerics {
 
     public static <T extends Comparable<T>> T testMaximum(T... values) {
-        Arrays.sort(values);
-        return values[values.length - 1];
+        T max = values[0];
+
+        for (T value : values) {
+            if (value.compareTo(max) > 0) {
+                max = value;
+            }
+        }
+        printMax(max);
+        return max;
+    }
+
+    public static <T> void printMax(T max) {
+        System.out.println("Maximum Value: " + max);
     }
 
     public static void main(String[] args) {
 
-        System.out.println(testMaximum(10, 40, 20, 30));
-        System.out.println(testMaximum("Apple", "Banana", "Peach", "Orange"));
+        testMaximum(10, 20, 30, 40);
+        testMaximum("Apple", "Peach", "Banana");
     }
 }
