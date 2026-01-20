32c7445b62130c5586528444080c867b8a7e73cf
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-20 20:07:44 +0530

    UC2:Last Name Validation using regex

--- advance_java/Day19/UserRegistration.java
+++ advance_java/Day19/UserRegistration.java
@@ -4,15 +4,15 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter First Name: ");
-        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
 
         String regex = "^[A-Z][a-z]{2,}$";
 
-        if (firstName.matches(regex)) {
-            System.out.println("Valid First Name");
+        if (lastName.matches(regex)) {
+            System.out.println("Valid Last Name");
         } else {
-            System.out.println("Invalid First Name");
+            System.out.println("Invalid Last Name");
         }
     }
 }
