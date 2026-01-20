48ca9519c80e847258e22eb113aaba64bf5f7990
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-20 20:18:16 +0530

    UC7:Add Rule1,Rule2,Rule3 password validation (min 8 chars,at least one Uppercase and number)

--- advance_java/Day19/UserRegistration.java
+++ advance_java/Day19/UserRegistration.java
@@ -6,12 +6,12 @@ public class UserRegistration {
 
         System.out.print("Enter Password: ");
         String password = sc.nextLine();
-        String regex = "^(?=.*[A-Z]).{8,}$";
+        String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
 
         if (password.matches(regex)) {
-            System.out.println("Password valid as per Rule 2");
+            System.out.println("Password is valid (Rules 1, 2, 3 passed)");
         } else {
-            System.out.println("Password invalid as per Rule 2");
+            System.out.println("Password is invalid");
         }
     }
 }
