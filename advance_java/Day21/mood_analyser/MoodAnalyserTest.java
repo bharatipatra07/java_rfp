b24aabd7bb452fec75d83f1050f3b96a1737d37a
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-21 22:19:05 +0530

    UC1-TC1.1:return SAD in given i am in Sad Mood

--- advance_java/Day21/mood_analyser/MoodAnalyserTest.java
+++ advance_java/Day21/mood_analyser/MoodAnalyserTest.java
@@ -3,6 +3,11 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
 
-    MoodAnalyser analyser = new MoodAnalyser();
+    @Test
+    void givenSadMood_ShouldReturnSAD() {
+        MoodAnalyser analyser = new MoodAnalyser();
+        String mood = analyser.analyseMood("I am in Sad Mood");
+        Assertions.assertEquals("SAD", mood);
+    }
 
 }
