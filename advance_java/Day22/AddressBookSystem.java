00230f83f862e37d10b0c6f7e68654f6ea37e76d
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-22 10:42:36 +0530

    UC2: Add ability to create new contact via console input

--- advance_java/Day22/AddressBookSystem.java
+++ advance_java/Day22/AddressBookSystem.java
@@ -1,6 +1,7 @@
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 class Contact {
 
     private String firstName;
@@ -53,20 +54,49 @@ public class AddressBookSystem {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
 
+        System.out.println("Enter First Name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter Address:");
+        String address = scanner.nextLine();
+
+        System.out.println("Enter City:");
+        String city = scanner.nextLine();
+
+        System.out.println("Enter State:");
+        String state = scanner.nextLine();
+
+        System.out.println("Enter Zip:");
+        String zip = scanner.nextLine();
+
+        System.out.println("Enter Phone Number:");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Enter Email:");
+        String email = scanner.nextLine();
+
         Contact contact = new Contact(
-                "Bharati",
-                "Patra",
-                "MG Road",
-                "Bangalore",
-                "Karnataka",
-                "560001",
-                "9876543210",
-                "bharati@gmail.com"
+                firstName,
+                lastName,
+                address,
+                city,
+                state,
+                zip,
+                phoneNumber,
+                email
         );
 
         addressBook.addContact(contact);
+
+        System.out.println("Contact Added Successfully!");
         addressBook.displayContacts();
+
+        scanner.close();
     }
 }
