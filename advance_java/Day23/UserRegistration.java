3c3917aab3e1c8c0c09fc73fc7f1345e082c1bce
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-22 14:25:23 +0530

    UC7:Add password validation rule for at least one numeric digit

--- advance_java/Day23/UserRegistration.java
+++ advance_java/Day23/UserRegistration.java
@@ -9,14 +9,18 @@ public class UserRegistration {
         UserInputValidator hasUpperCase =
                 password -> password.matches(".*[A-Z].*");
 
+        UserInputValidator hasNumber =
+                password -> password.matches(".*[0-9].*");
+
         UserInputValidator passwordValidator =
                 password -> password.length() >= 8
-                        && hasUpperCase.validate(password);
+                        && hasUpperCase.validate(password)
+                        && hasNumber.validate(password);
 
         // Test cases
         System.out.println(passwordValidator.validate("Pass@123")); // true
-        System.out.println(passwordValidator.validate("pass@123")); // false
-        System.out.println(passwordValidator.validate("PASSWORD")); // true
+        System.out.println(passwordValidator.validate("Password")); // false
+        System.out.println(passwordValidator.validate("pass1234")); // false
         System.out.println(passwordValidator.validate("Pass12"));   // false
     }
     }
