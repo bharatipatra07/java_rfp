91fb83ae7f3c4bfcd754d94ba0b449755acf6ccc
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-22 13:23:30 +0530

    UC6:Add password validation rule for at least one uppercase letter

--- advance_java/Day23/UserRegistration.java
+++ advance_java/Day23/UserRegistration.java
@@ -6,15 +6,19 @@ interface UserInputValidator {
 public class UserRegistration {
 
     public static void main(String[] args) {
+        UserInputValidator hasUpperCase =
+                password -> password.matches(".*[A-Z].*");
+
         UserInputValidator passwordValidator =
-                password -> password.length() >= 8;
+                password -> password.length() >= 8
+                        && hasUpperCase.validate(password);
 
         // Test cases
         System.out.println(passwordValidator.validate("Pass@123")); // true
+        System.out.println(passwordValidator.validate("pass@123")); // false
+        System.out.println(passwordValidator.validate("PASSWORD")); // true
         System.out.println(passwordValidator.validate("Pass12"));   // false
-        System.out.println(passwordValidator.validate("12345678")); // true
-        System.out.println(passwordValidator.validate("abc"));      // false
-
+    }
     }
 }
 
