c4a487c2644f15f9171670f2da9fc2c7c19dc0d6
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-23 11:54:19 +0530

    UC2: Add ability to create new contact via console input

--- advance_java/Day24/AddressBookSystem.java
+++ advance_java/Day24/AddressBookSystem.java
@@ -1,24 +1,52 @@
 package Day24;
-
+import java.util.Scanner;
 
 public class AddressBookSystem {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
 
+        System.out.println("Enter First Name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter Address:");
+        String address = scanner.nextLine();
+
+        System.out.println("Enter City:");
+        String city = scanner.nextLine();
+
+        System.out.println("Enter State:");
+        String state = scanner.nextLine();
+
+        System.out.println("Enter Zip:");
+        String zip = scanner.nextLine();
+
+        System.out.println("Enter Phone Number:");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Enter Email:");
+        String email = scanner.nextLine();
+
         Contact contact = new Contact(
-                "Kishor",
-                "Kumar",
-                "MG Road",
-                "Bangalore",
-                "Karnataka",
-                "560001",
-                "9876543210",
-                "kishor@gmail.com"
+                firstName,
+                lastName,
+                address,
+                city,
+                state,
+                zip,
+                phoneNumber,
+                email
         );
 
         addressBook.addContact(contact);
+
+        System.out.println("\nContact Added Successfully!\n");
         addressBook.displayContacts();
+        scanner.close();
     }
 }
