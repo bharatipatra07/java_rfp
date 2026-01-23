c194eb64f5b5f9317dab8c25a8671e6ee2f2a3d0
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-23 12:33:12 +0530

    UC5:Ability to add multiple contact to AddressBook via console input

--- advance_java/Day24/AddressBook.java
+++ advance_java/Day24/AddressBook.java
@@ -1,6 +1,5 @@
 package Day24;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 class AddressBook {
@@ -9,26 +8,8 @@ class AddressBook {
 
     public void addContact(Contact contact) {
         contacts.add(contact);
+        System.out.println("Contact added successfully.\n");
     }
-
-    // DELETE BY NAME
-    public boolean deleteContact(String firstName) {
-
-        Iterator<Contact> iterator = contacts.iterator();
-
-        while (iterator.hasNext()) {
-            Contact contact = iterator.next();
-            if (contact.getFirstName().equalsIgnoreCase(firstName)) {
-                iterator.remove();
-                System.out.println("Contact deleted successfully.");
-                return true;
-            }
-        }
-
-        System.out.println("Invalid first name");
-        return false;
-    }
-
 
     public void showContacts() {
         if (contacts.isEmpty()) {
--- advance_java/Day24/AddressBookSystem.java
+++ advance_java/Day24/AddressBookSystem.java
@@ -8,48 +8,53 @@ public class AddressBookSystem {
         Scanner scanner = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
 
-        // ---- Add Contact ----
-        System.out.println("Enter First Name:");
-        String firstName = scanner.nextLine();
+        boolean addMore = true;
 
-        System.out.println("Enter Last Name:");
-        String lastName = scanner.nextLine();
+        while (addMore) {
 
-        System.out.println("Enter Address:");
-        String address = scanner.nextLine();
+            System.out.println("Enter First Name:");
+            String firstName = scanner.nextLine();
 
-        System.out.println("Enter City:");
-        String city = scanner.nextLine();
+            System.out.println("Enter Last Name:");
+            String lastName = scanner.nextLine();
 
-        System.out.println("Enter State:");
-        String state = scanner.nextLine();
+            System.out.println("Enter Address:");
+            String address = scanner.nextLine();
 
-        System.out.println("Enter Zip:");
-        String zip = scanner.nextLine();
+            System.out.println("Enter City:");
+            String city = scanner.nextLine();
 
-        System.out.println("Enter Phone:");
-        String phone = scanner.nextLine();
+            System.out.println("Enter State:");
+            String state = scanner.nextLine();
 
-        System.out.println("Enter Email:");
-        String email = scanner.nextLine();
+            System.out.println("Enter Zip:");
+            String zip = scanner.nextLine();
 
-        addressBook.addContact(new Contact(
-                firstName, lastName, address,
-                city, state, zip, phone, email
-        ));
+            System.out.println("Enter Phone:");
+            String phone = scanner.nextLine();
 
-        System.out.println("Contact Added");
+            System.out.println("Enter Email:");
+            String email = scanner.nextLine();
 
-        // ---- DELETE CONTACT ----
-        System.out.println("Enter First Name to Delete:");
-        String nameToDelete = scanner.nextLine();
-        boolean deleted = addressBook.deleteContact(nameToDelete);
+            Contact contact = new Contact(
+                    firstName, lastName, address,
+                    city, state, zip, phone, email
+            );
 
-       // ---- Display ----
-        if (deleted) {
-            System.out.println("Address Book Cleared");
-            addressBook.showContacts();
+            addressBook.addContact(contact);
+
+            System.out.println("Do you want to add another person? (yes/no)");
+            String choice = scanner.nextLine();
+
+            if (!choice.equalsIgnoreCase("yes")) {
+                addMore = false;
+            }
         }
+
+        // ---- Display All Contacts ----
+        System.out.println("All Contacts in Address Book:");
+        addressBook.showContacts();
+
         scanner.close();
     }
 }
