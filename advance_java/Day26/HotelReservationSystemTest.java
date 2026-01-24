b856a013fe0a89aae43dc9c3c02e472175f90804
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-24 11:19:29 +0530

    UC2: Find cheapest hotel for given date range

--- advance_java/Day26/HotelReservationSystemTest.java
+++ advance_java/Day26/HotelReservationSystemTest.java
@@ -1,23 +1,30 @@
-import org.junit.jupiter.api.BeforeEach;
+package Day26;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
 
-package Day26;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HotelReservationSystemTest {
+class HotelReservationSystemUC2Test {
 
-    private HotelReservationSystem system;
+    @Test
+    void givenDateRange_whenFindingCheapestHotel_thenReturnLakewood() {
+        HotelReservationSystem system = new HotelReservationSystem();
 
-    @BeforeEach
-    void setup() {
-        system = new HotelReservationSystem();
+        system.addHotel(new Hotel("Lakewood", 3, 110, 90));
+        system.addHotel(new Hotel("Bridgewood", 4, 160, 60));
+        system.addHotel(new Hotel("Ridgewood", 5, 220, 150));
 
-        system.addHotel(new Hotel("Lakewood", 3, 110, 80, 90, 80));
-        system.addHotel(new Hotel("Bridgewood", 4, 160, 110, 60, 50));
-        system.addHotel(new Hotel("Ridgewood", 5, 220, 100, 150, 40));
+        List<LocalDate> dates = List.of(
+                LocalDate.of(2020, 9, 10),
+                LocalDate.of(2020, 9, 11)
+        );
+
+        Hotel cheapestHotel = system.findCheapestHotel(dates);
+        int totalRate = cheapestHotel.calculateTotalRate(dates);
+
+        assertEquals("Lakewood", cheapestHotel.getName());
+        assertEquals(220, totalRate);
     }
-
 }
