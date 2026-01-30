9380863dd3de9ddead603ad49c4fb4b82202900c
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-01-30 17:22:20 +0530

    UC2_TC1.4: Throw INVALID_FILE_FORMAT for State Code CSV with incorrect delimiter

--- io_streams/DesignPrinciple/StateCensusAnalyserTest.java
+++ io_streams/DesignPrinciple/StateCensusAnalyserTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class StateCensusAnalyserTest {
 
     /**
-     * TC1.3
+     * TC1.4
      * Given the State Code CSV file
-     * When file type is correct but data format is incorrect
+     * When delimiter is incorrect
      * Then it should throw INVALID_FILE_FORMAT exception
      */
     @Test
-    public void givenCSVFileWithIncorrectType_WhenLoaded_ShouldThrowInvalidFileFormatException() {
+    public void givenCSVFileWithIncorrectDelimiter_WhenLoaded_ShouldThrowInvalidFileFormatException() {
 
         StateCensusAnalyser analyser = new StateCensusAnalyser();
 
