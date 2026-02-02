f2c026165572ed58592ee36c62a170bd0224838e
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-02-02 17:24:44 +0530

    UC2:add employee_payroll table to payroll_service database

--- io_streams/MySQL/EmployeePayroll.sql
+++ io_streams/MySQL/EmployeePayroll.sql
@@ -1,5 +1,11 @@
-SHOW DATABASES;
-CREATE DATABASE payroll_service;
-SHOW DATABASES;
+-- Switch to payroll service database
 USE payroll_service;
 
+-- Create employee payroll table
+CREATE TABLE employee_payroll (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    start_date DATE NOT NULL,
+    PRIMARY KEY (id)
+);
