f6c744ccb4de041e6815e8207a7386ff2ab1d9d6
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-05-27 16:37:04 +0530

    EMAIL-UC5:Completed regex validation for email with optional username and country code

--- web_intro/ObjectClass/Regex.js
+++ web_intro/ObjectClass/Regex.js
@@ -1,9 +1,9 @@
-// UC4: Validate email with optional xyz and optional .in
+// Final Email Validation Program
 
 let email = "abc.xyz@bridgelabz.co.in";
 
 let emailRegex =
-/^[a-z]{3}([._+-][a-z]{3})?@[a-z]{5,}\.[a-z]{2}(\.[a-z]{2})?$/;
+/^[a-z]{3}([._+-][a-z]{3,})?@[a-z]{5,}\.[a-z]{2,}(\.[a-z]{2})?$/;
 
 if (emailRegex.test(email)) {
     console.log("Valid Email");
