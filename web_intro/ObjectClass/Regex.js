d849561aacc76810875531b7f0e684a1ac6a5ff7
bharatipatra07 <bharati.patra@bridgelabz.com>
2026-05-27 16:36:15 +0530

    EMAIL-UC4:Added validation for optional email username extension and country code

--- web_intro/ObjectClass/Regex.js
+++ web_intro/ObjectClass/Regex.js
@@ -1,8 +1,9 @@
-// UC3: Validate email with mandatory .co
+// UC4: Validate email with optional xyz and optional .in
 
-let email = "abc@bridgelabz.co";
+let email = "abc.xyz@bridgelabz.co.in";
 
-let emailRegex = /^[a-z]{3}@[a-z]{5,}\.[a-z]{2}$/;
+let emailRegex =
+/^[a-z]{3}([._+-][a-z]{3})?@[a-z]{5,}\.[a-z]{2}(\.[a-z]{2})?$/;
 
 if (emailRegex.test(email)) {
     console.log("Valid Email");
